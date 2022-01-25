16b9c14f1a27860c1caa28bf4f0383c2405849cb
bartosz-gruszczyk <bartosz.gruszczyk@gmail.com>
2022-01-25 15:41:07 +0100

    first draft of methods

--- homework/unique_ptr/main.cpp
+++ homework/unique_ptr/main.cpp
@@ -3,11 +3,29 @@
 
 int main() {
 
-    int a = 32;
+    // int a = 32;
     MyUniquePtr<int> myptr1(new int(3));
 
     std::cout << "myptr1 = " << *myptr1 << '\n';
-    // std::cout << "myptr1 = " << *myptr1 << '\n';
+
+    MyUniquePtr<int> myptr3(new int{3});
+    // myptr3 = new int{4};
+    // myptr3 = myptr1;
+    if (myptr3) {
+        std::cout << "myptr3 is not nullptr\n";
+    } else {
+        std::cout << "myptr3 is nullptr\n";
+    }
+    MyUniquePtr<int> myptr2(std::move(myptr1));
+    // std::cout << std::boolalpha << "myptr1 ? " << myptr1 << '\n';
+    std::cout << "myptr2 = " << *myptr2 << '\n';
+    MyUniquePtr<int> myptr4;
+    myptr4 = std::move(myptr2);
+    // std::cout << "myptr2 = " << *myptr2 << '\n';
+    std::cout << "mypt4 = " << *myptr4 << '\n';
+    myptr4 = new int{98};
+    std::cout << "mypt4 = " << *myptr4 << '\n';
+
 
 
     return 0;
--- homework/unique_ptr/myuniqueptr.hpp
+++ homework/unique_ptr/myuniqueptr.hpp
@@ -5,6 +5,23 @@ class MyUniquePtr {
 public:
     MyUniquePtr() {}
     MyUniquePtr(T* pointer) : pointer_{pointer} {}
+    MyUniquePtr(const MyUniquePtr<T>& other) = delete;
+    MyUniquePtr& operator=(const MyUniquePtr<T>& other) = delete;
+
+    MyUniquePtr(MyUniquePtr<T>&& other) { 
+        pointer_ = other.release();
+    }
+
+    MyUniquePtr& operator=(MyUniquePtr<T>&& other) {
+        if (other != *this) {
+            pointer_ = other.release();
+        }
+        return *this;
+    }
+
+    ~MyUniquePtr() {
+        delete pointer_;
+    }
 
     T operator*() {
         return *pointer_;
@@ -13,10 +30,42 @@ public:
     T operator->() {
         return *pointer_;
     }
+
+    T* get() const {  // const?
+        return pointer_;
+    }
+
+    T* release() {
+        T* tempPointer = pointer_;
+        pointer_ = nullptr;
+        return tempPointer;
+    }
+
+    void reset(T* other) { //should I check if it's another MyUnique?
+        delete pointer_;
+        pointer_ = other;
+    }
+
+    operator bool() {
+        return pointer_ != nullptr;
+    }
+
+    bool operator!=(const MyUniquePtr<T>& other) {
+        return pointer_ != other.get();
+    }
+
+    bool operator==(const MyUniquePtr<T>& other) {
+        return pointer_ == other.get();
+    }
+
     // get()
     // release()
+    
+
     // reset()
 
+    //operator bool
+    //operator << ?
 
 private:
     T* pointer_ = nullptr;
